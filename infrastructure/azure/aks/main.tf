94607216f8a1927480d5246bb39b2a4f22d2eccf
Bhuvan Ravi <bhuvanravi09@gmail.com>
2024-11-05 16:56:53 -0500

    changes to yaml fildddd

--- infrastructure/azure/aks/main.tf
+++ infrastructure/azure/aks/main.tf
@@ -3,6 +3,7 @@ provider "azurerm" {
   # subscription_id = "c20c5619-881c-43b4-ba94-880f0d2f333e"
 }
 
+# terrascan: ignore
 resource "azurerm_resource_group" "default" {
   name     = "${var.name}-${var.environment}-rg"
   location = var.location
